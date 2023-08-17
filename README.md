a17c5a5e51c09875271a8ea690f062cc3c810057
Rajeshwari8513 <105580174+Rajeshwari8513@users.noreply.github.com>
2023-08-17 13:01:33 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Combination-Ex-1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-mog57q)
---
+++ index.js
@@ -0,0 +1,25 @@
+let dayOfWeek = "Saturday";
+
+switch (dayOfWeek){
+    case "Monday" :
+    case "Tuesday" :
+    case "Wednesday" :
+    case "Thrusday" :
+    
+if(dayOfWeek === "Wednesday") {
+    console.log("Attend a meeting")
+}else{
+    console.log("Goto work")
+}
+break;
+
+    case "Friday" :
+    console.log("Have some fun")
+break;
+
+case "Saturday" :
+case "Sunday" :
+console.log("Enjoyed a week-end")
+break
+    
+}
